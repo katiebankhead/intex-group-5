6a3ac03efc05c3a350f6f0fabd819ed799541c23
broadkent <broadkent@gmail.com>
2020-04-06 18:11:30 -0600

    added animated thingie

--- src/hero.js
+++ src/hero.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import { Container, Navbar, Nav, Form, FormControl, Button, Jumbotron } from 'react-bootstrap'
+import React from "react";
+import { Container, Navbar, Nav, Form, FormControl, Button, Jumbotron } from "react-bootstrap";
+import LottieControl from "./lottie"
 
 export default function Hero() {
-    return (
-        <Container className='py-10  d-flex flex-column align-items-center'>
-            <img className="m-5" src='./go-fund-me-logo.png' alt='logo' style={{width: '500px'}}/>
-            <p>Kent's awesome animation goes here</p>
-            <div className='d-flex'>
-                <Button block className='m-4 rounded-pill' style={{ backgroundColor: "#009758", borderColor: "#009758", width: '20rem', height: '4rem' }} size="lg">
-                    TRAINING
-                </Button>
-                <Button block className='m-4 rounded-pill' style={{ backgroundColor: "#009758", borderColor: "#009758", width: '20rem' }} size="lg">
-                    LAUNCH
-                </Button>
-            </div>
-            <br />
-        </Container>
-    )
+
+
+  return (
+    <Container className='py-10  d-flex flex-column align-items-center'>
+      <LottieControl />
+      <div className='d-flex'>
+        <Button
+          block
+          className='m-4 rounded-pill'
+          style={{
+            backgroundColor: "#009758",
+            borderColor: "#009758",
+            width: "20rem",
+            height: "4rem",
+          }}
+          size='lg'>
+          TRAINING
+        </Button>
+        <Button
+          block
+          className='m-4 rounded-pill'
+          style={{ backgroundColor: "#009758", borderColor: "#009758", width: "20rem" }}
+          size='lg'>
+          LAUNCH
+        </Button>
+      </div>
+      <br />
+    </Container>
+  );
 }
---
+++ src/lottie.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+import * as animationData from './logo_animated.json'
+import { Container } from 'react-bootstrap'
+
+export default class LottieControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isStopped: false, isPaused: false }
+  }
+  render() {
+    const defaultOptions = {
+      loop: false,
+      autoplay: true,
+      animationData: animationData.default,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYmid slice'
+      }
+    }
+    return (<Container>
+      <Lottie options={defaultOptions}
+        height={"30rem"} />
+    </Container>)
+  }
+}

4d011f0f8281e8796c13102d2f6833ed2d583200
broadkent <broadkent@gmail.com>
2020-04-07 15:24:20 -0600

    Change home background to light gray

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 //import all the components
-import TopContainer from './top-container'
-import Home from './home'
-import Calculator from './calculator'
-import Campaigns from './campaigns';
+import TopContainer from "./top-container";
+import Home from "./home";
+import Calculator from "./calculator";
+import Campaigns from "./campaigns";
 
 function App() {
   return (
     <Router>
       <Container fluid className='p-0 m-0 min-vh-100 d-flex flex-column'>
-        <Row noGutters className='flex-grow-0 flex-shrink-0 shadow'>
-          <Col className='px-3 py-2' style={{ backgroundColor: '#fff' }}>
+        <Row noGutters className='flex-grow-0 flex-shrink-0'>
+          <Col className='px-3 py-2' style={{ backgroundColor: "#fff" }}>
             <TopContainer />
           </Col>
         </Row>
         <Row noGutters className='flex-grow-1'>
-          <Col md='12'>
+          <Col md='12' style={{ backgroundColor: "#F3F3F3" }}>
             <Switch>
               <Route path='/calculator'>
                 <Calculator />
@@ -26,7 +26,7 @@ function App() {
               <Route path='/campaigns'>
                 <Campaigns />
               </Route>
-              <Route path="/">
+              <Route path='/'>
                 <Home />
               </Route>
             </Switch>
